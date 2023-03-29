8dd3ee9acdbc53e287b878fef0acb27704300dbe
Julius Czar <juliusczar36@gmail.com>
2023-03-29 22:59:58 +0300

    Write a class Square that defines a square by: (based on 1-square.py)

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" Module to define a square of size 'size' """
+
+
+class Square:
+    """class that defines a square"""
+    def __init__(self, size=0):
+        """ class Square with a private attribute class
+
+        Args:
+             size (int): size of the new square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
