e36a12d2782f630d29c68c6a7eb42b7276220907
CzarProCoder <juliusczar36@gmail.com>
2023-04-05 01:49:02 +0300

    Write a function def copy_list(l): that returns a copy of a list

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+
+"""
+Module to create a copy of a list
+
+Args:
+    l (list): List to be cloned
+
+Returns:
+    Returns a copy of the list l
+"""
 def copy_list(l):
     """ Function to create a copy of list l"""
     return (l[:])
