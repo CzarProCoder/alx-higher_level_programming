0030962aa7a9d74f8a26d193243a7818eee78dff
CzarProCoder <juliusczar36@gmail.com>
2023-04-05 23:10:51 +0300

    Solution to: Write an empty class Rectangle that defines a rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module to define a rectangle class
+"""
+
+
+class Rectangle:
+    """
+    Class defining a rectangle
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+README.md file for ALX-Holberton school for 0x08. Python - More Classes and Objects project
