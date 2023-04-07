1ce345a57a27ba4532e08fca9160f82a648e00ff
CzarProCoder <juliusczar36@gmail.com>
2023-04-07 20:19:50 +0300

    Fixed bugs on on file 0-add_integer.py and tests/0-add_integer.txt

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -40,6 +40,6 @@ def add_integer(a, b=98):
     """
     if not isinstance (a, (float, int)):
         raise TypeError("a must be an integer")
-    if not isinstance (a, (float, int)):
+    if not isinstance (b, (float, int)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
