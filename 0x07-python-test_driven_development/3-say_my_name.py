1767e723ea17791d42b462076856d4a24d5605f1
CzarProCoder <juliusczar36@gmail.com>
2023-04-08 23:26:12 +0300

    function that prints My name is <first name> <last name>

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 """
+Module for function to print names
 """
+
+
 def say_my_name(first_name, last_name=""):
     """
+    Function to calculate print the first and last_name
+
+    Args:
+        first_name: First name to be printed
+        last_name: Last_name to be printed
     """
-    if not  isinstance(first_name, str):
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
