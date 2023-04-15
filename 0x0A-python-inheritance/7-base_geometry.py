3f8521bfbcf6b326a042143f5499fdcabb77891a
CzarProCoder <juliusczar36@gmail.com>
2023-04-15 17:05:07 +0300

    A class BaseGeometry (based on 6-base_geometry.py) with Public instance method: def integer_validator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -26,4 +26,4 @@ class BaseGeometry():
         if type(value) != int:
             raise TypeError("{:s} must be an integer".format(name))
         elif (value <= 0):
-            raise ValueError(f"{:s} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
