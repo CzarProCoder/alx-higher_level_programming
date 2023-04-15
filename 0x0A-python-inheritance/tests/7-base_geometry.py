053ffb1bf59981c45f8af58ef32dfe78a22a1649
CzarProCoder <juliusczar36@gmail.com>
2023-04-15 16:48:56 +0300

    A class BaseGeometry (based on 6-base_geometry.py) with Public instance method: def integer_validator

---
+++ 0x0A-python-inheritance/tests/7-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Module containing a class BaseGeometry.
+"""
+
+
+class BaseGeometry():
+    """
+    Calculations class
+    """
+    def area(self):
+        """
+        No implemented
+        """
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """
+        Function to validate a value
+
+        Args:
+            name (string): String to be used
+            value (int): Integer to be evaluated
+        """
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        elif (value <= 0):
+            raise ValueError(f"{name} must be greater than 0")
