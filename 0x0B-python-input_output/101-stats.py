bf3053ed00d747b6bff62d9e4357140220411b54
CzarProCoder <juliusczar36@gmail.com>
2023-04-19 17:20:18 +0300

    Script that reads stdin line by line and computes metrics

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -16,6 +16,59 @@ format: <status code (in ascending order)>: <number>
 status codes should be printed in ascending order
 """
 
+
+def print_stats(size, status_codes):
+    """
+    Print accumulated metrics
+    
+    Args:
+        size (int): Accumulated read file size.
+        status_codes (dict): Accumulated count of status codes.
+    """
+    print(f"File size: {size}")
+    for key in sorted(status_codes):
+        print(f"{key}: {status_codes[key]}")
+
+if __name__ == "__main__":
+    import sys
+
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
+
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
+
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
+
+
+"""
 import sys
 
 
@@ -48,3 +101,4 @@ def parse_stdin_and_compute():
 
 
 parse_stdin_and_compute()
+    """
