9f65547071bbcc8fd688e85bbee0ce9771c7b8b1
CzarProCoder <juliusczar36@gmail.com>
2023-04-25 02:13:36 +0300

    Created a class Rectange (subclass of Base)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,13 +5,14 @@ Module that defines Rectangle, a subclass of Base
 """
 from models.base import Base
 
+
 class Rectangle(Base):
     """
     class Rectangle, that inherits from Base
     """
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ Initializes a class Rectangle 
-        
+        """Initializes a class Rectangle
+
         Args:
             width (int): Width of the rectangle
             height (int): Height of the rectangle
@@ -38,3 +39,45 @@ class Rectangle(Base):
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
+
+    @property
+    def height(self):
+        """ Get the width of the rectangle """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Set the width of the rectangle """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """ Get the width of the rectangle """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ Set the width of the rectangle """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value <= 0:
+            raise ValueError("x must be > 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Get the width of the rectangle """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ Set the width of the rectangle """
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value <= 0:
+            raise ValueError("y must be > 0")
+        self.__y = value
