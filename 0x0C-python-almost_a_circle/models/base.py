3984c33dc435233035aacddfb050545be114f9d4
CzarProCoder <juliusczar36@gmail.com>
2023-04-26 12:43:19 +0300

    Updated the class Base by adding class methods

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@
 Module to define class Base
 """
 import json
+import csv
 
 
 class Base():
@@ -91,20 +92,37 @@ class Base():
         except IOError:
             return []
 
+    @classmethod
     def save_to_file_csv(cls, list_objs):
-        """
-        Serializes and deserializes in CSV
-        """
+        "Serializes to cvs"
         filename = cls.__name__ + ".csv"
-        filename = cls.__name__ + ".csv"
-        with open(filename, "w", newline="") as csvfile:
-            if list_objs is None or list_objs == []:
-                csvfile.write("[]")
-            else:
+        with open(filename, 'w', newline='') as f:
+            writer = csv.writer(f)
+            for o in list_objs:
                 if cls.__name__ == "Rectangle":
-                    fieldnames = ["id", "width", "height", "x", "y"]
-                else:
-                    fieldnames = ["id", "size", "x", "y"]
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                for obj in list_objs:
-                    writer.writerow(obj.to_dictionary())
+                    writer.writerow([o.id, o.width, o.height, o.x, o.y])
+                if cls.__name__ == "Square":
+                    writer.writerow([o.id, o.size, o.x, o.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        "Deserializes from csv"
+        obs = []
+        filename = cls.__name__ + ".csv"
+        with open(filename, mode='r', newline='') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if cls.__name__ == "Rectangle":
+                    dic = {"id": int(row[0]),
+                           "width": int(row[1]),
+                           "height": int(row[2]),
+                           "x": int(row[3]),
+                           "y": int(row[4])}
+                if cls.__name__ == "Square":
+                    dic = {"id": int(row[0]),
+                           "size": int(row[1]),
+                           "x": int(row[2]),
+                           "y": int(row[3])}
+                o = cls.create(**dic)
+                obs.append(o)
+        return obs
