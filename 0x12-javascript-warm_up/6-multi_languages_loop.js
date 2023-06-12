5f9c306ca90420573366abd9861c752d1b064f93
CzarProCoder <juliusczar36@gmail.com>
2023-06-12 21:27:04 +0000

    Created an array to print lines in JS

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const l of lines) {
   console.log(l);
 }
