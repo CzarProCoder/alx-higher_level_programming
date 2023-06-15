c5b255e4cbd493c1a156f0a80c47cd1e784a3904
CzarProCoder <juliusczar36@gmail.com>
2023-06-15 13:16:26 +0300

    Created a Js script to map list items

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-
-const list = require('./100-data').list;
-
+const list = require('./100-data.js').list;
 console.log(list);
-console.log(list.map((x, index) => x * index));
+console.log(list.map((item, index) => item * index));
