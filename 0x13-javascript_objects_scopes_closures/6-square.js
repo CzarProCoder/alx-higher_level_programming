8ec3820b3d6c8ac9f6571c764363cf9285b24b32
CzarProCoder <juliusczar36@gmail.com>
2023-06-15 12:06:34 +0300

    Added a new Square class to print a rectangle in new style

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const Sq = require('./5-square');
+
+class Square extends Sq {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
