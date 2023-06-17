e08ca55d4d891270529c6747f49d4483c16c3325
CzarProCoder <juliusczar36@gmail.com>
2023-06-17 08:07:25 -0700

    Script that prints the states as per the arguments taken

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 '''
+Script that takes in the name of a state as an argument
+	and lists all cities of that state
 
+The script should take 4 arguments: mysql username,
+	mysql password, database name and state name
 '''
 
 import sys
@@ -10,7 +14,7 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute(SELECT * FROM states WHERE name = sys.argv[4])
+    cur.execute("""SELECT * FROM states WHERE name = sys.argv[4]""")
     states = cur.fetchall{}
     for state in states:
         print(state)
