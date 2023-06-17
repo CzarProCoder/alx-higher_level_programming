d1945baf7e9479ed2fe66d35f61b16d6483f6588
CzarProCoder <juliusczar36@gmail.com>
2023-06-17 21:52:52 +0300

    Created a script that contains the class definition of a State

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 
 '''
+Python file that contains the class definition of a State
+and an instance Base = declarative_base()
 '''
 
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
+
+
+class State(Base):
+    '''
+    Inherits from Base class
+    '''
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
