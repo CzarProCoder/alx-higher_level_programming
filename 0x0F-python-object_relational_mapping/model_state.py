d191d287ecf63718e55972306aeb346ee7ddcde5
CzarProCoder <juliusczar36@gmail.com>
2023-06-17 22:19:16 +0300

    Created a script that contains the class definition of a State

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 '''
-Python file that contains the class definition of a State
+Script that contains the class definition of a State
 and an instance Base = declarative_base()
 '''
 
