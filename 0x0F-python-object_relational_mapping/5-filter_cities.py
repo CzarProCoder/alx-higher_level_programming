418b540d48a2cb4396d5451ea38900d3e524c1f1
CzarProCoder <juliusczar36@gmail.com>
2023-06-17 21:13:36 +0300

    Wrote a script that takes in the name of a state as an argument and lists all cities of that state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,17 +11,17 @@ mysql password, database name and state name
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
+
     cur = db.cursor()
-    cur.execute("""SELECT cities.name
-                 FROM states
-                 INNER JOIN cities ON states.id = cities.state_id
-                 WHERE states.name LIKE %s
-                 ORDER BY cities.id ASC""")
+    cur.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id;")
     states = cur.fetchall()
+
     for state in states:
         print(state)
+
     cur.close()
     db.close()
