57855fd11a8b6ab295a96a84f46dfa14ac67f570
CzarProCoder <juliusczar36@gmail.com>
2023-06-17 13:52:49 +0300

    Created a script that check the name of script from a 4th argv

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+'''
+script that takes in an argument and displays all values in
+    the states table of hbtn_0e_0_usa where name
+    matches the argument
+
+Script should take 4 arguments: mysql username,
+    mysql password, database name and state name searched
+'''
+
+import sys
+import MySQLdb
+
+if __name__ = "__main__":
+    db = MySQLdb(user=sys.argv[1], passwd=sys.argv[2],
+                 db=sys.argv[3], stateName=sys.argv[4], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name=stateName ORDER BY id ASC")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+
+    cur.close()
+    db.close()
