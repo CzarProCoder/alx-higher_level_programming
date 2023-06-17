97ddbbed2459998a249f19765d7f7d5db5b3440e
CzarProCoder <juliusczar36@gmail.com>
2023-06-17 21:09:03 +0300

    Wrote a script that takes in the name of a state as an argument and lists all cities of that state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,7 +12,8 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("""SELECT cities.name
                  FROM states
