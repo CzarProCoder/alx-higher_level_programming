8b847caff24fb7126c944f890e3f3bb107f1fd95
CzarProCoder <juliusczar36@gmail.com>
2023-06-17 07:16:51 -0700

    Added a script that lists all cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,11 +13,11 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name
+    cur.execute("""SELECT cities.id, cities.name, states.name
                 FROM cities
                 JOIN states
                 ON cities.state_id=states.id
-                ORDER BY cities.id ASC")
+                ORDER BY cities.id ASC""")
     states = cur.fetchall()
     for state in states:
         print(state)
