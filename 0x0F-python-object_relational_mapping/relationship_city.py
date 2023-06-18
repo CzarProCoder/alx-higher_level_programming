6bc35f1b034ed924379095fe800eeab1c3c35839
CzarProCoder <juliusczar36@gmail.com>
2023-06-18 10:55:00 -0700

    Introduced scripts relationship_city.py and relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -11,13 +11,13 @@ Base = declarative_base()
 
 
 class City(Base):
-    '''Represents a city for a MySQL database.
+    """Represents a city for a MySQL database.
 
     Attributes:
-        id (str): The city's id.
-        name (sqlalchemy.Integer): The city's name.
-        state_id (sqlalchemy.String): The city's state id.
-    '''
+        id (sqlalchemy.Column): The city's id.
+        name (sqlalchemy.Column): The city's name.
+        state_id (sqlalchemy.Column): The city's state id.
+    """
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
