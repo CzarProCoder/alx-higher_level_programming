893fecd16ab688b0e465da2bc5080b32f7f64cb8
CzarProCoder <juliusczar@gmail.com>
2023-07-02 12:47:21 -0700

    Added script comments

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/bash
-# Script to take URL, send request to the URL an
-#	displays the side of the response
-
+# Script to take URL, send request to the URL and displays the side of the response
 curl -s "$1" | wc -c
