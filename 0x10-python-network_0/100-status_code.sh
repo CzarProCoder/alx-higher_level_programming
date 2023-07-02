9ede1a401dfc36ce7512b8e970c6ff9ee7626abb
CzarProCoder <juliusczar@gmail.com>
2023-07-02 14:48:12 -0700

    Created a script to send a request and display the status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# ends a request to a URL passed as an argument,
-# and displays only the status code of the response
+# Sends a request to a URL passed as an argument, and displays only the status code of the response
 curl -s -o /dev/null -w "%{http_code}" "$1"
