98a48575e64ead52a916e76aff8b4e00b7a7746b
CzarProCoder <juliusczar@gmail.com>
2023-07-02 14:39:14 -0700

    Added a script to find the peak in a list of integers

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+''' Script that finds the peak of unsorted list '''
+
+
+def find_peak(list_of_integers):
+    '''
+    Function to find the peak element in a list of 5 elements
+
+    Args:
+        List of integers
+    '''
+    if list_of_integers == []:
+        return None
+
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
