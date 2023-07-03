faf73e163385109d9c4b719f573aebd28bf510b7
CzarProCoder <juliusczar36@gmail.com>
2023-07-03 12:31:45 -0700

    Created a script that takes in a url and email and displays the body of response

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,5 +10,5 @@ from sys import argv
 if __name__ == "__main__":
     url = argv[1]
     email = argv[2]
-    r = requests.post(url, data{'email': email})
+    r = requests.post(url, data={'email': email})
     print(r.text)
