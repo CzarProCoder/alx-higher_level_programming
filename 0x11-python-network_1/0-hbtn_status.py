7495c15712af6cfbdcea5cfbd01710e9bfafaced
CzarProCoder <juliusczar36@gmail.com>
2023-07-03 13:27:18 +0300

    Created a python script that fetches the content of a website

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,8 @@ Python script that fetches the url https://alx-intranet.hbtn.io/status
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
+            as response:
         html = response.read()
 
         print("Body reponse:")
