d6323c72a3e83739fe3964a771898afc0aa02657
CzarProCoder <juliusczar36@gmail.com>
2023-07-03 03:49:29 -0700

    Made the file executable

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,13 +4,10 @@ Python script that fetches the url https://alx-intranet.hbtn.io/status
 '''
 
 import urllib.request
-
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
-            as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-
-        print("Body reponse:")
+        print('Body response:')
         print(f"\t- type: {type(html)}")
         print(f"\t- content: {html}")
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print(f"\t- utf8 content: {html.decode('utf-8')}")
