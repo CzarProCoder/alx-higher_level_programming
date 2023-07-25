9024b74cb99896fbdd5a528d737aceaaad18317a
CzarProCoder <juliusczar36@gmail.com>
2023-07-25 21:24:49 +0000

    Wrote a script that computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,20 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
-
-request(url, (err, resp, body) => {
-  if (err) { console.log(err); }
-
-  const completed = {};
-  const jsonBody = JSON.parse(body);
-  for (const task of jsonBody) {
-    if (task.completed) {
-      if (completed[task.userId]) {
-        completed[task.userId]++;
-      } else {
-        completed[task.userId] = 1;
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
-    }
+    });
+    console.log(completed);
   }
-  console.log(completed);
 });
