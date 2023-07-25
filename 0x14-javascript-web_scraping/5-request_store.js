21121610764d7dd60bb45a7cab7e17ef9d9288a2
CzarProCoder <juliusczar36@gmail.com>
2023-07-25 21:19:35 +0000

    Made the file executable

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const request = require('request')
+const request = require('request');
 const fs = require('fs');
-const url = process.argv[2]
+const url = process.argv[2];
 const filename = process.argv[3];
 
 request(url).pipe(fs.createWriteStream(filename));
