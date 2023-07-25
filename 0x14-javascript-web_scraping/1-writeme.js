029b3ab8931a9d443cb0bf4c71019914920a993b
CzarProCoder <juliusczar36@gmail.com>
2023-07-25 13:16:09 +0000

    Made the file executable

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,20 +1,7 @@
 #!/usr/bin/node
-
-/*
-JS script to write a string to a file:
-The first argument is the file path
-The second argument is the string to write
-The content of the file must be written in utf-8
-If an error occurred during while writing, print the error object
-*/
-
-import ps from 'ps';
-
-const filename = process.argv[1];
-const text = process.argv[2];
-
-ps.writeFile(filename, text, 'utf8', function (err) {
-  if (err) {
-    console.log(err);
+const fs = require('fs');
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) {
+    console.log(error);
   }
 });
